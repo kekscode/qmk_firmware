96244ba13fdae2d1cf6992f9cdaa7e921a79d88f
Stefan Antoni <stefan.antoni@deutschebahn.com>
2020-11-30 19:22:14 +0100

    Switch alt and ctrl for better thumb ergonomics

--- keyboards/kyria/keymaps/kekscode/keymap.c
+++ keyboards/kyria/keymaps/kekscode/keymap.c
@@ -11,20 +11,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(
       KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T,                                                 KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
       LT(_RAISE, KC_TAB), KC_A, KC_S, KC_D, KC_F, KC_G,                                     KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_ENT,
-      KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, SGUI(KC_5), LGUI(LCTL(KC_Q)),                  KC_NO, LCTL(KC_BSLS), KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_LSFT,
-           KC_LCTL, KC_LALT, LT(_LOWER, KC_SPC), KC_LGUI, LT(_ADJUST, KC_F1),          LT(_ADJUST, KC_F1), KC_LGUI, LT(_LOWER, KC_SPC), KC_LALT, KC_LCTL
+      KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, SGUI(KC_5), LGUI(LCTL(KC_Q)),                  KC_NO, KC_NO, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_LSFT,
+           KC_LALT, KC_LCTL, LT(_LOWER, KC_SPC), KC_LGUI, LT(_ADJUST, KC_F1),          LT(_ADJUST, KC_F1), KC_LGUI, LT(_LOWER, KC_SPC), KC_LCTL, LCTL(KC_BSLS) 
     ),
     [_LOWER] = LAYOUT(
      KC_ESC, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC,                                           KC_CIRC, KC_AMPR, KC_ASTR, KC_MINS, KC_PLUS, KC_DEL,
      KC_TAB, KC_NO, KC_UNDS, KC_LCBR, KC_LPRN, KC_QUOT,                                          KC_DQUO, KC_RPRN, KC_RCBR, KC_MINS, KC_PIPE, KC_ENT,
      KC_LSFT, KC_NO, KC_NO, KC_LT, KC_LBRC, KC_TILD, KC_NO, KC_NO,             KC_NO, KC_NO, KC_GRV, KC_RBRC, KC_GT, KC_NO, KC_BSLS, KC_LSFT, 
-                     KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, KC_NO,                 KC_NO, KC_SPC, KC_VOLD, KC_VOLU, KC_LCTL
+                     KC_LALT, KC_LCTL, KC_LGUI, KC_SPC, KC_NO,                 KC_NO, KC_SPC, KC_VOLD, KC_VOLU, KC_LALT
     ),
     [_RAISE] = LAYOUT(
             KC_NO, KC_LEFT, KC_UP, KC_NO, KC_NO, KC_NO,                                  KC_PLUS, KC_7, KC_8, KC_9, KC_DEL, KC_BSPC,
             KC_NO, KC_PGUP, KC_DOWN, KC_RGHT, KC_NO, KC_NO,                                 KC_EQL, KC_4, KC_5, KC_6, KC_ASTR, KC_ENT,
             KC_LSFT, KC_NO, KC_NO, KC_PGDN, KC_NO, KC_NO, KC_NO, KC_NO,       KC_NO, KC_NO, KC_MINS, KC_1, KC_2, KC_3, KC_SLSH, KC_LSFT,
-                            KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, KC_NO,           KC_NO, KC_SPC, KC_0, KC_COMM, KC_DOT
+                            KC_LALT, KC_LCTL, KC_LGUI, KC_SPC, KC_NO,           KC_NO, KC_SPC, KC_0, KC_COMM, KC_DOT
     ),
     [_ADJUST] = LAYOUT(
       KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6,                                                    KC_F7,   KC_F8,   KC_F9,   KC_F10, KC_F11, KC_F12,
