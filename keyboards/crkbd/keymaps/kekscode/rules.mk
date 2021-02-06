738db475128460df0cca69c0a753fca6ce36ae6c
Stefan Antoni <stefan.antoni@deutschebahn.com>
2021-02-06 19:07:33 +0100

    Add rules

--- keyboards/crkbd/keymaps/kekscode/rules.mk
+++ keyboards/crkbd/keymaps/kekscode/rules.mk
@@ -1,1 +1,5 @@
-OLED_DRIVER_ENABLE  = yes
+OLED_DRIVER_ENABLE  = yes   # Enables the use of OLED displays
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+LEADER_ENABLE = no          # Enable the Leader Key feature
+ENCODER_ENABLE = no         # Enables the use of one or more encoders
+MOUSEKEY_ENABLE = no        # Enables mousekey support
