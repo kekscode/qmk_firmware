e18ffac743ec76d5f93c86f7a5568e752ca6c9ca
Stefan Antoni <stefan.antoni@deutschebahn.com>
2021-02-06 18:53:57 +0100

    Next layer done

--- keyboards/crkbd/keymaps/kekscode/keymap.c
+++ keyboards/crkbd/keymaps/kekscode/keymap.c
@@ -34,15 +34,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [1] = LAYOUT_split_3x6_3(
-  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_TAB,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_BSPC,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, XXXXXXX, XXXXXXX,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI, _______,  KC_SPC,     KC_ENT,   MO(3), KC_RALT
-                                      //`--------------------------'  `--------------------------'
+     KC_ESC, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC,                                           KC_CIRC, KC_AMPR, KC_ASTR, KC_MINS, KC_PLUS, KC_DEL,
+     KC_TAB, KC_NO, KC_UNDS, KC_LCBR, KC_LPRN, KC_QUOT,                                          KC_DQUO, KC_RPRN, KC_RCBR, KC_MINS, KC_PIPE, KC_ENT,
+     KC_LSFT, KC_NO, KC_NO, KC_LT, KC_LBRC, KC_TILD,                                             KC_GRV, KC_RBRC, KC_GT, KC_NO, KC_BSLS, KC_LSFT, 
+                            KC_LCTL, KC_LALT, KC_LGUI,                                           KC_NO, KC_NO, KC_NO
   ),
 
   [2] = LAYOUT_split_3x6_3(
