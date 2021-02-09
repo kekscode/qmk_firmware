90a9183ace530a88b0331e2df23243272dc76db8
Stefan Antoni <stefan.antoni@deutschebahn.com>
2021-02-09 12:05:18 +0100

    Reformat

--- keyboards/crkbd/keymaps/kekscode/keymap.c
+++ keyboards/crkbd/keymaps/kekscode/keymap.c
@@ -1,6 +1,7 @@
 /*
 Copyright 2019 @foostan
 Copyright 2020 Drashna Jaelre <@drashna>
+Copyright 2021 @kekscode
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -20,32 +21,32 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_split_3x6_3(
-       KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
-      LT(2, KC_TAB),    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,             KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_ENT,
-      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_LSFT,
-                                          KC_LCTL,   KC_LGUI,  KC_LALT,     LCTL(KC_BSLS), LT(1, KC_SPC), LT(3, KC_F1)
+          KC_ESC,        KC_Q, KC_W, KC_E, KC_R, KC_T,                 KC_Y,          KC_U,          KC_I,        KC_O,   KC_P,    KC_BSPC,
+          LT(2, KC_TAB), KC_A, KC_S, KC_D, KC_F, KC_G,                 KC_H,          KC_J,          KC_K,        KC_L,   KC_SCLN, KC_ENT,
+          KC_LSFT,       KC_Z, KC_X, KC_C, KC_V, KC_B,                 KC_N,          KC_M,          KC_COMM,     KC_DOT, KC_SLSH, KC_LSFT,
+                               KC_LCTL, KC_LGUI, KC_LALT,              LCTL(KC_BSLS), LT(1, KC_SPC), LT(3, KC_F1)
 
   ),
 
   [1] = LAYOUT_split_3x6_3(
-     KC_ESC, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC,                                           KC_CIRC, KC_AMPR, KC_ASTR, KC_MINS, KC_PLUS, KC_DEL,
-     KC_TAB, KC_NO, KC_UNDS, KC_LCBR, KC_LPRN, KC_QUOT,                                          KC_DQUO, KC_RPRN, KC_RCBR, KC_MINS, KC_PIPE, KC_ENT,
-     KC_LSFT, KC_NO, KC_NO, KC_LT, KC_LBRC, KC_TILD,                                             KC_GRV, KC_RBRC, KC_GT, KC_NO, KC_BSLS, KC_LSFT, 
-                            KC_LCTL, KC_LGUI, KC_LALT,                                           KC_NO, KC_NO, KC_NO
+          KC_ESC,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,        KC_CIRC, KC_AMPR, KC_ASTR, KC_MINS, KC_PLUS, KC_DEL,
+          KC_TAB,  KC_NO,   KC_UNDS, KC_LCBR, KC_LPRN, KC_QUOT,        KC_DQUO, KC_RPRN, KC_RCBR, KC_MINS, KC_PIPE, KC_ENT,
+          KC_LSFT, KC_NO,   KC_NO,   KC_LT,   KC_LBRC, KC_TILD,        KC_GRV,  KC_RBRC, KC_GT,   KC_NO,   KC_BSLS, KC_LSFT, 
+                                     KC_LCTL, KC_LGUI, KC_LALT,        KC_NO,   KC_NO,   KC_NO
   ),
 
   [2] = LAYOUT_split_3x6_3(
-            KC_NO, KC_PGUP, KC_UP, KC_PGDN, KC_NO, KC_NO,                                  KC_PLUS, KC_7, KC_8, KC_9, KC_DEL, KC_BSPC,
-            KC_NO, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO, KC_NO,                                 KC_EQL, KC_4, KC_5, KC_6, KC_ASTR, KC_ENT,
-            KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                   KC_MINS, KC_1, KC_2, KC_3, KC_SLSH, KC_LSFT,
-                            KC_LCTL, KC_LGUI, KC_LALT,                                    KC_SPC, KC_0, KC_DOT
+          KC_NO,   KC_PGUP, KC_UP,   KC_PGDN, KC_NO,   KC_NO,          KC_PLUS, KC_7, KC_8, KC_9, KC_DEL,  KC_BSPC,
+          KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,   KC_NO,          KC_EQL,  KC_4, KC_5, KC_6, KC_ASTR, KC_ENT,
+          KC_LSFT, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,          KC_MINS, KC_1, KC_2, KC_3, KC_SLSH, KC_LSFT,
+                                     KC_VOLD, KC_VOLU, KC_MUTE,        KC_SPC,  KC_0, KC_DOT
   ),
 
   [3] = LAYOUT_split_3x6_3(
-      KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6,                                                    KC_F7,   KC_F8,   KC_F9,   KC_F10, KC_F11, KC_F12,
-      KC_NO, KC_VOLD, KC_MUTE, KC_VOLU, KC_NO, KC_NO,                                              KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                                    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-                           KC_NO, KC_NO, KC_NO,                                                    KC_NO, KC_NO, KC_NO
+          KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6,                    KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,
+          KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                    KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO,  KC_NO,
+          KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                    KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO,  KC_NO,
+                               KC_NO, KC_NO, KC_NO,                    KC_NO, KC_NO, KC_NO
   )
 };
 
